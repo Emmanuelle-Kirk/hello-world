e6a98fa4c1d594dc6fbfc8ad40a3a52727b9385c
Emmanuelle-Kirk <wolfeetried@gmail.com>
2021-09-14 10:45:45 -0300

    Initial commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!"); //display text
+            Console.ReadKey();
+            Console.WriteLine("Keep pressing buttons"); //display text
+            Console.ReadKey(); //wait for user input
+            Console.WriteLine("Almost there!"); //display text
+            Console.ReadKey(); //wait for user input
+            Console.WriteLine("Keep it up!");
+            Console.ReadKey();
+            Console.WriteLine("One more press!");
+            Console.ReadKey(); //wait for user input
+        }
+    }
+}
